516032d8246aa0e22b29acbf8b10f8205518835b
Pepo <pepo@uprtcl.io>
2020-11-17 15:44:59 +0100

    first commit

---
+++ env.js
@@ -0,0 +1,45 @@
+// const peerPath = `/dns4/localhost/tcp/4003/ws/p2p`;
+// const peerId = "QmTJHMJzwD5je8tu6YgfJ9tBK6Aac9zqvCoimsLvtibnry";
+// const env = {
+//   entry: "./src/index.eth.orbitdb.js",
+//   officialRemote: "eth",
+//   pinner: {
+//     url: "http://localhost:3100",
+//     Swarm: [],
+//     Bootstrap: [`${peerPath}/${peerId}`],
+//     peerMultiaddr: `${peerPath}/${peerId}`,
+//   },
+//   ethers: {
+//     apiKeys: {
+//       etherscan: "6H4I43M46DJ4IJ9KKR8SFF1MF2TMUQTS2F",
+//       infura: "73e0929fc849451dae4662585aea9a7b",
+//     },
+//     provider: "",
+//     // provider: 'https://xdai.poanetwork.dev'
+//   },
+// };
+
+const peerPath = `/dns4/pinner.intercreativity.io/tcp/4003/wss/p2p`;
+const peerId = "QmVD8LC6vjAHaDgsLySc86BVbnb256LuRZqsWtK5toABsc";
+
+const env = {
+  entry: "./src/index.eth.orbitdb.js",
+  officialRemote: "eth",
+  pinner: {
+    url: "https://apps.intercreativity.io:3000",
+    Swarm: [],
+    Bootstrap: [`${peerPath}/${peerId}`],
+    peerMultiaddr: `${peerPath}/${peerId}`,
+  },
+  ethers: {
+    apiKeys: {
+      etherscan: "6H4I43M46DJ4IJ9KKR8SFF1MF2TMUQTS2F",
+      infura: "73e0929fc849451dae4662585aea9a7b",
+    },
+    provider: "https://xdai.poanetwork.dev",
+  },
+};
+
+module.exports = {
+  env,
+};
